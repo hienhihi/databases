ce1516650b46850e9cba95c8af5e783753cee48a
hienhihi <emails.hienn@gmail.com>
2019-11-13 20:54:34 -0800

    Add tables to chat schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,12 +1,41 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id int(11) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+);
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int(11) NOT NULL AUTO_INCREMENT,
+  user_id int(11),
+  message_text MEDIUMTEXT,
+  room_name VARCHAR(50),
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id int(11) NOT NULL AUTO_INCREMENT,
+  room_name VARCHAR(50),
+  message_id int,
+  user_id int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (message_id) REFERENCES messages (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE friends (
+  /* Describe your table here.*/
+  user_id int(11) NOT NULL,
+  friend_id int(11) NOT NULL
+);
 
 
 
